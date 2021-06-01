87230e02f98452355912572737147c6a36c548de
Rahul Tiwari <rahultwr0005@gmail.com>
2021-06-01 19:49:15 +0530

    second improvisation v1.1

--- public/JS/Home.js
+++ public/JS/Home.js
@@ -56,18 +56,20 @@ window.onload = function() {
 };
 
 const popUp = document.querySelector(".popUp")
-const contentBody = document.querySelector(".content-body")
+// const contentBody = document.querySelector(".content-body")
 document.querySelector("#contact-btn").addEventListener("click", () => {
-    var classes = popUp.className.split(" ")
-    let i = classes.indexOf("hide")
-    if(i >= 0) {
-        classes.splice(i, 1);
-    }
-    else {
-        classes.push("hide");
-    }
-    popUp.className = classes.join(" ");
+    // var classes = popUp.className.split(" ")
+    // let i = classes.indexOf("hide")
+    // if(i >= 0) {
+    //     classes.splice(i, 1);
+    // }
+    // else {
+    //     classes.push("hide");
+    // }
+    // popUp.className = classes.join(" ");
     // alert("hlw")
+    popUp.classList.toggle("hide")
+    contentBody.style.overflow = "hidden"
     contentBody.style.filter = "blur(2px)"
 })
 
@@ -75,5 +77,12 @@ contentBody.addEventListener("click", () => {
     if(!popUp.classList.contains("hide")) {
         popUp.classList.toggle("hide")
         contentBody.style.filter = "blur(0px)"
+        contentBody.style.overflow = "auto"
     }
 }, capture='True')
+
+
+// Feedback form scripting
+
+const feedbackForm = document.querySelector(".feedback-form")
+const feedBtn = document.querySelector(".feedback-form button")
